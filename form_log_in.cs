23540e905edcd073b544eb12cde9e7db56bddb0a
sal7799 <67878928+sal7799@users.noreply.github.com>
2021-05-26 21:02:45 +0300

    works

---
+++ form_log_in.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WinFormsApp1
+{
+    public partial class form_log_in : Form
+    {
+        public form_log_in()
+        {
+            InitializeComponent();
+        }
+        string connition = ("Data Source = DESKTOP-O3UH2S9\\SQLEXPRESS; initial catalog = ahmed ; integrated security=true");
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == "" && textBox2.Text == "")
+                {
+                    MessageBox.Show("write pass and name");
+                }
+                else
+                {
+                    SqlConnection sqlcon = new SqlConnection(connition);
+                    SqlCommand cmd = new SqlCommand("select * from Table_1 where userName= @usero and password=@passo", sqlcon);
+                    cmd.Parameters.AddWithValue("@usero", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@passo", textBox2.Text);
+                    sqlcon.Open();
+                    SqlDataAdapter sqla = new SqlDataAdapter(cmd);
+                    DataSet dt = new DataSet();
+                    sqla.Fill(dt);
+                    sqlcon.Close();
+
+                    int count = dt.Tables[0].Rows.Count;
+                    if (count == 1)
+                    {
+                        MessageBox.Show("logedin");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Not");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            form_log_up a = new form_log_up();
+            a.Show();
+
+        }
+
+        private void form_log_in_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ form_log_up.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+//using System.Data.commandType;
+
+namespace WinFormsApp1
+{
+    public partial class form_log_up : Form
+    {
+        public form_log_up()
+        {
+            InitializeComponent();
+        }
+        string connition = ("Data Source = DESKTOP-O3UH2S9\\SQLEXPRESS; initial catalog = ahmed ; integrated security=true");
+        SqlCommand cmd;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sql = new SqlConnection(connition);
+            sql.Open();
+            SqlCommand cmd = new SqlCommand("insert into Table_1 values ('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "')",sql);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("data saved");
+            sql.Close();
+            select();
+        }
+        private void select()
+        {
+           
+            SqlConnection sqlc = new SqlConnection(connition);
+            sqlc.Open();
+            SqlCommand cmd =new SqlCommand("readData", sqlc);
+            cmd.CommandType= CommandType.StoredProcedure;
+           // SqlParameter[] par = new SqlParameter();
+            
+            SqlDataAdapter sqlaa = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sqlaa.Fill(dt);
+            dataGridView1.DataSource = dt;
+            sqlc.Close();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void form_log_up_Load(object sender, EventArgs e)
+        {
+            select();
+        }
+    }
+}
